bb0e18ef5888566c5cdb762635bda4944a2867e9
SamKlep <shamdemann23@gmail.com>
2020-11-06 14:16:09 -0600

    Tracking correct files.

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/ui/Header'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <div className='container mt-4 text-center'>
+          <h1>Serial Killers</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
