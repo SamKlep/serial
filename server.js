f60528afc1fab23584259333dd4c57d37a7c8e07
SamKlep <shamdemann23@gmail.com>
2020-11-09 16:51:37 -0600

    Prod build route.

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build')) // serve the static react app
   app.get(/^\/(?!api).*/, (req, res) => {
     // don't serve api routes to react app
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
   })
   console.log('Serving React App...')
 }
