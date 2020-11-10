e4552c1def58d3eb14a316efdc99839f913c1157
SamKlep <shamdemann23@gmail.com>
2020-11-10 17:26:35 -0600

    Psychologu page content.

--- frontend/src/components/screens/PsychologyScreen.js
+++ frontend/src/components/screens/PsychologyScreen.js
@@ -66,14 +66,235 @@ const PsychologyScreens = () => {
           <Col>
             <div>
               <p>
-                Keeping in mind the idea that infant bonding is imperative to
-                personality development, it is no surprise that a common
-                characteristic of serial killers is that they were adopted
-                (Leyton 1987). This may imply that as infants serial killers
-                such as, David Berkowitz, Joel Rifkin, and Kenneth Bianchi did
-                not receive proper attention as they were given up by their
-                biological mothers for adoption during a vital period in their
-                lives.
+                Childhood trauma has also been linked to serial killers. Studies
+                have revealed a number of significant statistics supporting the
+                idea that mental and physical trauma may have long lasting
+                effects upon an individual (Aynesworth 1999). 42% of convicted
+                serial killers suffered from physical abuse as children 74%
+                suffered from psychological abuse (Aynesworth 1999). Sexual
+                abuse also seemed to be a prominent characteristic among serial
+                killers as 35% witnessed sexual abuse and 43% were sexually
+                abused themselves (Aynesworth 1999). Another form of physical
+                trauma linked to serial killers is head trauma. 29% of serial
+                killers were found to be ‘accident prone children’ (Aynesworth
+                1999). The compilation of these factors leads many sociologists
+                to view psychopathic behavior as the brain’s defense mechanism
+                (Aynesworth 1999).
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container className='mx-auto w-75 mt-5 mb-5'>
+        <Row>
+          <Col>
+            <div>
+              <p>
+                As does infancy, early childhood is key to the formation of a
+                serial killer. Commonly, serial killers were isolated from their
+                peers as children (Vronsky 2004). Many serial killers suffered
+                from weight problems, stutters and learning problems such as
+                dyslexia (Moesch 1998). Due to bullying, many of these serial
+                killers began harboring “secret aggressive fantasies” (Vronsky
+                2004). As stated by David Berkowitz also known as the Son of
+                Sam, “It was a mysterious force working against me. I felt
+                bothered and tormented, ‘Die Schmutz’ [Yiddish for ‘the dirty
+                one’].” (Vronsky 2004). It is clear that aggression begins in
+                early childhood. Loneliness, as illustrated, may lead to the
+                evolution of fantasies as these ‘serial killers in- the- making’
+                are frequently alone.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container className='mx-auto w-75 mt-5 mb-5'>
+        <Row>
+          <Col>
+            <div>
+              <p>
+                Another common characteristic among serial killers is their
+                relationship with their mother. The mothers of these ‘monsters’
+                tend to breed men that hate females (Moesch 1998). The mothers
+                may be over controlling, overprotective, physically abusive and
+                emotionally abusive (Moesch 1998). According to one
+                psychologist, Sigmund Freud, males try to reach autonomy with
+                their mother. If the child is unsuccessful this will result in
+                rage (Moesch 1998). Many feminists disdain the idea of the
+                mother assisting in the creation of a serial killer. Feminists
+                argue that it is just another way for men to blame women for
+                their downfalls, somewhat of a ‘blame it on the mother” scenario
+                (Moesch 1998). This notion is empirically supported to an extent
+                however as 66% of known serial killers were raised with the
+                mother as the dominant figure (Moesch 1998).
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container className='d-flex mt-5'>
+        <Row>
+          <Col>
+            <div>
+              <p>
+                Childhood trauma has also been linked to serial killers. Studies
+                have revealed a number of significant statistics supporting the
+                idea that mental and physical trauma may have long lasting
+                effects upon an individual (Aynesworth 1999). 42% of convicted
+                serial killers suffered from physical abuse as children 74%
+                suffered from psychological abuse (Aynesworth 1999). Sexual
+                abuse also seemed to be a prominent characteristic among serial
+                killers as 35% witnessed sexual abuse and 43% were sexually
+                abused themselves (Aynesworth 1999). Another form of physical
+                trauma linked to serial killers is head trauma. 29% of serial
+                killers were found to be ‘accident prone children’ (Aynesworth
+                1999). The compilation of these factors leads many sociologists
+                to view psychopathic behavior as the brain’s defense mechanism
+                (Aynesworth 1999).
+              </p>
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <p>
+                The emphasis upon infancy and early childhood may align serial
+                killers with the Biological Trait Theories. These theories focus
+                upon biological conditions that may control human behaviors.
+                Neurophysiology is the study of brain function. It has uncovered
+                neurological and physical abnormalities that may begin as early
+                as the prenatal stage in some humans. There have been links
+                between the impairment of executive brain function and
+                aggression through such research (Aynesworth 1999). Many of the
+                serial killers discussed have been labeled with some sort of
+                neurological disorder. Thus, these repetitive serial killers may
+                just be reacting to chemicals and hereditary factors in their
+                brains rather than on pure evil.
+              </p>
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <p>
+                Another Criminological theory that may apply to serial killers
+                is the Psychodynamic theory. According to Sigmund Freud the
+                development of the unconscious personality early in childhood
+                will influence behavior for the rest of one’s life. In short,
+                negative experiences act as” residue” from early childhood.
+                Freud believes that the human develops, early in life, three
+                aspects of their personality. These include the id, the ego and
+                the superego (Seltzer 1995). The id is considered to be
+                primitive, supplying unconscious drives for food and sex. The
+                ego is formed as sort of a guide to remain aligned with
+                societies norms. Finally the superego develops incorporating
+                values and morals. Thus, serial killers seem to be overwhelmed
+                by their id. This would explain why people such as David
+                Berkowitz would become a serial killer. He did not receive
+                proper care as an infant and child, most likely due to his
+                adoption, thus he was negatively affected into adulthood.
+                According to the psychodynamic theory crime occurs when a person
+                displays a weak ego. In short, serial killers seem to be
+                manifesting their feelings of oppression from childhood in the
+                form of brutal murders.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container className='mx-auto w-75 mt-5 mb-5'>
+        <Row>
+          <Col>
+            <div>
+              <p>
+                According to this theory the organized offender is thought of as
+                leading an organized, average life. He is also thought to have
+                average to above average intelligence, skilled employment and he
+                is socially competent. These characteristics are deciphered from
+                the crime scene left by an “organized offender”. The crime scene
+                of such an offender suggests that the killer planned his attack,
+                bringing a weapon with him as well as bringing it with him when
+                he leaves. The organized serial killer is also careful not to
+                leave blood and finger prints behind. The organized serial
+                killer is thought to be killing as some sort of reaction to a
+                stressful event (Canter 2004).
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container className='mx-auto w-75 mt-5 mb-5'>
+        <Row>
+          <Col>
+            <div>
+              <p>
+                In contrast to the organized serial killer there is the
+                disorganized serial killer according to this theory. The
+                disorganized killer leaves the crime scene just as the name
+                suggests; in disarray. The disorganized offender is likely to
+                leave blood, fingerprints and the murder weapon behind (Canter
+                2004). The killing is thought to be opportunistic and often
+                occurs close to where the offender lives (Canter 2004).
+                Characteristically, the disorganized killer is thought to be
+                socially inadequate and have below-average intelligence. They
+                are believed to struggle with maintaining social relationships
+                (Canter 2004). This aspect of the organized/disorganized theory
+                is somewhat similar to the idea of the social control theory.
+                This theory also states that limited stakes in conventional
+                society may lead to criminal behavior.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container className='mx-auto w-75 mt-5 mb-5'>
+        <Row>
+          <Col>
+            <div>
+              <p>
+                The psychology of the serial killer is intriguing yet
+                frightening. The decay of these offenders’ personalities begins
+                at infancy for many and continues through childhood and into
+                adulthood. The serial killer tends to be neglected during
+                significant stages of infancy, where in which the development of
+                the human personality begins. As these offenders enter childhood
+                they are still at a disadvantage socially, often as a result of
+                abuse and parental issues. Serial killers often lack self
+                control, as well as other critical characteristics of a law
+                abiding citizen. It seems as if socialization plays a large roll
+                in the formation of these monsters. A number of other
+                criminological theories can be applied to the serial killer in
+                some form, however there are still anomalies. The Biological
+                Trait Theories, Psychodynamic theory, and the
+                Organized/Disorganized theory all attempt to explain the
+                inexplicable actions of serial killers. The biological trait
+                theories apply as there is great emphasis on infancy and
+                childhood emotional growth. The psychodynamic theory applies as
+                well as these men display actions that seem to be driven by
+                their id personality. Lastly, the organized/disorganized theory
+                attempts to conclude personality and behavioral characteristics
+                from crime scene investigation.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container className='d-flex mt-5'>
+        <Row>
+          <Col>
+            <div>
+              <p>
+                The term ‘serial killer’ strikes fear deep into the hearts of
+                most. However it is often thought that fear is what drives some
+                curiosities, thus finding it appropriate to understand why
+                serial killers commit horrific acts of violence repetitively. It
+                has been found that many serial killers encompass similars
+                emotional development issues. They also tend to be above-average
+                intelligence wise, and in some ways it is as if killing acts as
+                a drug for some of these men. Many theories of crime and
+                deviance correlate to the actions of serial killers. Studies of
+                known serial killers have unveiled similarities between them as
+                early as infancy. Infancy, childhood and relationships with
+                one’s mother are often among the key parallels of serial killers
+                (Vronsky 2004).
               </p>
             </div>
           </Col>
