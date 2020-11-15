6a6324b687faa085e0f81030e4478345130027fb
SamKlep <shamdemann23@gmail.com>
2020-11-14 22:46:58 -0600

    Psychology page using grid system.

--- frontend/src/components/screens/PsychologyScreen.js
+++ frontend/src/components/screens/PsychologyScreen.js
@@ -12,26 +12,24 @@ const PsychologyScreens = () => {
         </p>
       </Jumbotron>
       <hr className='bg-secondary' />
-      <Container className='d-flex mt-5'>
+      <Container className='mt-5'>
         <Row>
-          <Col>
-            <div>
-              <p>
-                The term ‘serial killer’ strikes fear deep into the hearts of
-                most. However it is often thought that fear is what drives some
-                curiosities, thus finding it appropriate to understand why
-                serial killers commit horrific acts of violence repetitively. It
-                has been found that many serial killers encompass similars
-                emotional development issues. They also tend to be above-average
-                intelligence wise, and in some ways it is as if killing acts as
-                a drug for some of these men. Many theories of crime and
-                deviance correlate to the actions of serial killers. Studies of
-                known serial killers have unveiled similarities between them as
-                early as infancy. Infancy, childhood and relationships with
-                one’s mother are often among the key parallels of serial killers
-                (Vronsky 2004).
-              </p>
-            </div>
+          <Col lg={6}>
+            <p className='lead'>
+              The term ‘serial killer’ strikes fear deep into the hearts of
+              most. However it is often thought that fear is what drives some
+              curiosities, thus finding it appropriate to understand why serial
+              killers commit horrific acts of violence repetitively. It has been
+              found that many serial killers encompass similars emotional
+              development issues. They also tend to be above-average
+              intelligence wise, and in some ways it is as if killing acts as a
+              drug for some of these men. Many theories of crime and deviance
+              correlate to the actions of serial killers. Studies of known
+              serial killers have unveiled similarities between them as early as
+              infancy. Infancy, childhood and relationships with one’s mother
+              are often among the key parallels of serial killers (Vronsky
+              2004).
+            </p>
           </Col>
           <Col>
             <Image
@@ -41,6 +39,11 @@ const PsychologyScreens = () => {
               src='https://cdn.vox-cdn.com/thumbor/hFJai9flUoMebs-ftbhlUUO1iVg=/0x0:1764x1200/1200x800/filters:focal(881x473:1163x755)/cdn.vox-cdn.com/uploads/chorus_image/image/52090637/cover.0.png'
             />
           </Col>
+        </Row>
+      </Container>
+      <br />
+      <Container>
+        <Row>
           <Col>
             <div>
               <p>
@@ -61,6 +64,7 @@ const PsychologyScreens = () => {
           </Col>
         </Row>
       </Container>
+
       <Container className='mx-auto w-75 mt-5 mb-5'>
         <Row>
           <Col>
@@ -82,6 +86,22 @@ const PsychologyScreens = () => {
                 (Aynesworth 1999).
               </p>
             </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col lg={6}>
+            <Image
+              src='https://2zadtx2dccrd43bc0r18hhqj-wpengine.netdna-ssl.com/wp-content/uploads/sites/8/2018/12/rszserialkillermurderviole_1455814.jpg'
+              fluid
+            />
+          </Col>
+          <Col lg={6}>
+            <Image
+              src='https://crimeviral.com/wp-content/uploads/2020/01/Head-Injuries-Cover.jpg'
+              fluid
+            />
           </Col>
         </Row>
       </Container>
@@ -108,31 +128,37 @@ const PsychologyScreens = () => {
           </Col>
         </Row>
       </Container>
+      <hr className='bg-secondary' />
       <Container className='mx-auto w-75 mt-5 mb-5'>
         <Row>
-          <Col>
-            <div>
-              <p>
-                Another common characteristic among serial killers is their
-                relationship with their mother. The mothers of these ‘monsters’
-                tend to breed men that hate females (Moesch 1998). The mothers
-                may be over controlling, overprotective, physically abusive and
-                emotionally abusive (Moesch 1998). According to one
-                psychologist, Sigmund Freud, males try to reach autonomy with
-                their mother. If the child is unsuccessful this will result in
-                rage (Moesch 1998). Many feminists disdain the idea of the
-                mother assisting in the creation of a serial killer. Feminists
-                argue that it is just another way for men to blame women for
-                their downfalls, somewhat of a ‘blame it on the mother” scenario
-                (Moesch 1998). This notion is empirically supported to an extent
-                however as 66% of known serial killers were raised with the
-                mother as the dominant figure (Moesch 1998).
-              </p>
-            </div>
+          <Col lg={6}>
+            <Image
+              src='https://ca-times.brightspotcdn.com/dims4/default/f0b6100/2147483647/strip/true/crop/512x357+0+0/resize/840x586!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F62%2F33%2F8654c9e50092e20a18032dbe5e39%2Fsdut-file-in-this-july-30-1979-fi-20160904'
+              fluid
+            />
+          </Col>
+          <Col lg={6}>
+            <p>
+              Another common characteristic among serial killers is their
+              relationship with their mother. The mothers of these ‘monsters’
+              tend to breed men that hate females (Moesch 1998). The mothers may
+              be over controlling, overprotective, physically abusive and
+              emotionally abusive (Moesch 1998). According to one psychologist,
+              Sigmund Freud, males try to reach autonomy with their mother. If
+              the child is unsuccessful this will result in rage (Moesch 1998).
+              Many feminists disdain the idea of the mother assisting in the
+              creation of a serial killer. Feminists argue that it is just
+              another way for men to blame women for their downfalls, somewhat
+              of a ‘blame it on the mother” scenario (Moesch 1998). This notion
+              is empirically supported to an extent however as 66% of known
+              serial killers were raised with the mother as the dominant figure
+              (Moesch 1998).
+            </p>
           </Col>
         </Row>
       </Container>
-      <Container className='d-flex mt-5'>
+
+      <Container className=' mt-5'>
         <Row>
           <Col>
             <div>
@@ -154,7 +180,19 @@ const PsychologyScreens = () => {
               </p>
             </div>
           </Col>
-          <Col>
+        </Row>
+        <hr className='bg-secondary' />
+        <br />
+        <Row className='justify-content-center mb-5'>
+          <Col md={4}>
+            <Image
+              src='https://cdn.images.express.co.uk/img/dynamic/78/590x/Joseph-Roy-Metheny-838407.jpg'
+              fluid
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
             <div>
               <p>
                 The emphasis upon infancy and early childhood may align serial
@@ -172,7 +210,7 @@ const PsychologyScreens = () => {
               </p>
             </div>
           </Col>
-          <Col>
+          <Col md={6}>
             <div>
               <p>
                 Another Criminological theory that may apply to serial killers
@@ -200,103 +238,114 @@ const PsychologyScreens = () => {
           </Col>
         </Row>
       </Container>
-      <Container className='mx-auto w-75 mt-5 mb-5'>
+      <hr className='bg-secondary' />
+      <Container className='mx-auto mt-5 mb-5'>
         <Row>
+          <Col lg={6}>
+            <p>
+              According to this theory the organized offender is thought of as
+              leading an organized, average life. He is also thought to have
+              average to above average intelligence, skilled employment and he
+              is socially competent. These characteristics are deciphered from
+              the crime scene left by an “organized offender”. The crime scene
+              of such an offender suggests that the killer planned his attack,
+              bringing a weapon with him as well as bringing it with him when he
+              leaves. The organized serial killer is also careful not to leave
+              blood and finger prints behind. The organized serial killer is
+              thought to be killing as some sort of reaction to a stressful
+              event (Canter 2004).
+            </p>
+          </Col>
           <Col>
-            <div>
-              <p>
-                According to this theory the organized offender is thought of as
-                leading an organized, average life. He is also thought to have
-                average to above average intelligence, skilled employment and he
-                is socially competent. These characteristics are deciphered from
-                the crime scene left by an “organized offender”. The crime scene
-                of such an offender suggests that the killer planned his attack,
-                bringing a weapon with him as well as bringing it with him when
-                he leaves. The organized serial killer is also careful not to
-                leave blood and finger prints behind. The organized serial
-                killer is thought to be killing as some sort of reaction to a
-                stressful event (Canter 2004).
-              </p>
-            </div>
+            <Image
+              src='https://cdn.crimeandinvestigation.co.uk/s3fs-public/styles/landscape__tablet/public/smart_serial_killers3-min_1.jpg?timestamp=1563456977'
+              fluid
+            />
           </Col>
         </Row>
       </Container>
-      <Container className='mx-auto w-75 mt-5 mb-5'>
+      <Container className='mx-auto  mt-5 mb-5'>
         <Row>
+          <Col lg={6}>
+            <p>
+              In contrast to the organized serial killer there is the
+              disorganized serial killer according to this theory. The
+              disorganized killer leaves the crime scene just as the name
+              suggests; in disarray. The disorganized offender is likely to
+              leave blood, fingerprints and the murder weapon behind (Canter
+              2004). The killing is thought to be opportunistic and often occurs
+              close to where the offender lives (Canter 2004).
+              Characteristically, the disorganized killer is thought to be
+              socially inadequate and have below-average intelligence. They are
+              believed to struggle with maintaining social relationships (Canter
+              2004). This aspect of the organized/disorganized theory is
+              somewhat similar to the idea of the social control theory. This
+              theory also states that limited stakes in conventional society may
+              lead to criminal behavior.
+            </p>
+          </Col>
           <Col>
-            <div>
-              <p>
-                In contrast to the organized serial killer there is the
-                disorganized serial killer according to this theory. The
-                disorganized killer leaves the crime scene just as the name
-                suggests; in disarray. The disorganized offender is likely to
-                leave blood, fingerprints and the murder weapon behind (Canter
-                2004). The killing is thought to be opportunistic and often
-                occurs close to where the offender lives (Canter 2004).
-                Characteristically, the disorganized killer is thought to be
-                socially inadequate and have below-average intelligence. They
-                are believed to struggle with maintaining social relationships
-                (Canter 2004). This aspect of the organized/disorganized theory
-                is somewhat similar to the idea of the social control theory.
-                This theory also states that limited stakes in conventional
-                society may lead to criminal behavior.
-              </p>
-            </div>
+            <Image
+              src='https://images.unsplash.com/photo-1563486688110-3e8087cb4bb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80'
+              fluid
+            />
           </Col>
         </Row>
       </Container>
-      <Container className='mx-auto w-75 mt-5 mb-5'>
+      <Container className='mx-auto mt-5 mb-5'>
         <Row>
+          <Col lg={6}>
+            <p>
+              The psychology of the serial killer is intriguing yet frightening.
+              The decay of these offenders’ personalities begins at infancy for
+              many and continues through childhood and into adulthood. The
+              serial killer tends to be neglected during significant stages of
+              infancy, where in which the development of the human personality
+              begins. As these offenders enter childhood they are still at a
+              disadvantage socially, often as a result of abuse and parental
+              issues. Serial killers often lack self control, as well as other
+              critical characteristics of a law abiding citizen. It seems as if
+              socialization plays a large roll in the formation of these
+              monsters. A number of other criminological theories can be applied
+              to the serial killer in some form, however there are still
+              anomalies. The Biological Trait Theories, Psychodynamic theory,
+              and the Organized/Disorganized theory all attempt to explain the
+              inexplicable actions of serial killers. The biological trait
+              theories apply as there is great emphasis on infancy and childhood
+              emotional growth. The psychodynamic theory applies as well as
+              these men display actions that seem to be driven by their id
+              personality. Lastly, the organized/disorganized theory attempts to
+              conclude personality and behavioral characteristics from crime
+              scene investigation.
+            </p>
+          </Col>
           <Col>
-            <div>
-              <p>
-                The psychology of the serial killer is intriguing yet
-                frightening. The decay of these offenders’ personalities begins
-                at infancy for many and continues through childhood and into
-                adulthood. The serial killer tends to be neglected during
-                significant stages of infancy, where in which the development of
-                the human personality begins. As these offenders enter childhood
-                they are still at a disadvantage socially, often as a result of
-                abuse and parental issues. Serial killers often lack self
-                control, as well as other critical characteristics of a law
-                abiding citizen. It seems as if socialization plays a large roll
-                in the formation of these monsters. A number of other
-                criminological theories can be applied to the serial killer in
-                some form, however there are still anomalies. The Biological
-                Trait Theories, Psychodynamic theory, and the
-                Organized/Disorganized theory all attempt to explain the
-                inexplicable actions of serial killers. The biological trait
-                theories apply as there is great emphasis on infancy and
-                childhood emotional growth. The psychodynamic theory applies as
-                well as these men display actions that seem to be driven by
-                their id personality. Lastly, the organized/disorganized theory
-                attempts to conclude personality and behavioral characteristics
-                from crime scene investigation.
-              </p>
-            </div>
+            <Image
+              src='https://images.unsplash.com/photo-1572297880316-e7c2d45be117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+              fluid
+            />
           </Col>
         </Row>
       </Container>
-      <Container className='d-flex mt-5'>
+      <hr className='bg-secondary' />
+      <Container className='mt-5'>
         <Row>
           <Col>
-            <div>
-              <p>
-                The term ‘serial killer’ strikes fear deep into the hearts of
-                most. However it is often thought that fear is what drives some
-                curiosities, thus finding it appropriate to understand why
-                serial killers commit horrific acts of violence repetitively. It
-                has been found that many serial killers encompass similars
-                emotional development issues. They also tend to be above-average
-                intelligence wise, and in some ways it is as if killing acts as
-                a drug for some of these men. Many theories of crime and
-                deviance correlate to the actions of serial killers. Studies of
-                known serial killers have unveiled similarities between them as
-                early as infancy. Infancy, childhood and relationships with
-                one’s mother are often among the key parallels of serial killers
-                (Vronsky 2004).
-              </p>
-            </div>
+            <p>
+              The term ‘serial killer’ strikes fear deep into the hearts of
+              most. However it is often thought that fear is what drives some
+              curiosities, thus finding it appropriate to understand why serial
+              killers commit horrific acts of violence repetitively. It has been
+              found that many serial killers encompass similars emotional
+              development issues. They also tend to be above-average
+              intelligence wise, and in some ways it is as if killing acts as a
+              drug for some of these men. Many theories of crime and deviance
+              correlate to the actions of serial killers. Studies of known
+              serial killers have unveiled similarities between them as early as
+              infancy. Infancy, childhood and relationships with one’s mother
+              are often among the key parallels of serial killers (Vronsky
+              2004).
+            </p>
           </Col>
         </Row>
       </Container>
